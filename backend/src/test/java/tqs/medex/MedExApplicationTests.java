5fd3240cd167b73fffbc33c97f356ce359f406df
Daniel Gomes <dagomes@ua.pt>
2021-05-25 12:03:51 +0100

    initial backend POM

---
+++ backend/src/test/java/tqs/medex/MedExApplicationTests.java
@@ -0,0 +1,13 @@
+package tqs.medex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
