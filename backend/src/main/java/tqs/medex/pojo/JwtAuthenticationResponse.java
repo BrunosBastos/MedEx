18d26f38a86b6dcf7b7dcb12434cc7f7a8f0a6db
Leandro Silva <leandrosilva12@ua.pt>
2021-06-07 15:20:02 +0100

    added isSuperUser to login response

--- backend/src/main/java/tqs/medex/pojo/JwtAuthenticationResponse.java
+++ backend/src/main/java/tqs/medex/pojo/JwtAuthenticationResponse.java
@@ -8,8 +8,15 @@ import lombok.Setter;
 public class JwtAuthenticationResponse {
   private String accessToken;
   private String tokenType = "Bearer";
+  private boolean isSuperUser;
 
   public JwtAuthenticationResponse(String accessToken) {
     this.accessToken = accessToken;
+    this.isSuperUser = false;
+  }
+
+  public JwtAuthenticationResponse(String accessToken, boolean isSuperUser) {
+    this.accessToken = accessToken;
+    this.isSuperUser = isSuperUser;
   }
 }
