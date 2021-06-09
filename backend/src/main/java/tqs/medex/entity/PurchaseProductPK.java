7c5df86b62c97c3adac708e3ea8c9fcda17b1097
BrunosBastos <Brunosb@ua.pt>
2021-06-09 17:15:47 +0100

    removed useless functions

--- backend/src/main/java/tqs/medex/entity/PurchaseProductPK.java
+++ backend/src/main/java/tqs/medex/entity/PurchaseProductPK.java
@@ -15,16 +15,4 @@ public class PurchaseProductPK implements Serializable {
   private long product;
   private long purchase;
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    PurchaseProductPK that = (PurchaseProductPK) o;
-    return product == that.product && purchase == that.purchase;
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(product, purchase);
-  }
 }
