5fe0c6e645e508ee83b7e7ee09d88d60f8970d76
BrunosBastos <Brunosb@ua.pt>
2021-06-15 11:19:17 +0100

    fix production env url

--- projWebsite/src/config/index.ts
+++ projWebsite/src/config/index.ts
@@ -1,1 +1,9 @@
-export const MEDEX_API_BASE_URL: string = 'http://localhost:8080/api/v1/';
+export let MEDEX_API_BASE_URL: string = null;
+if (process.env.REACT_APP_RUNNING_MODE != null && process.env.REACT_APP_RUNNING_MODE == 'PRODUCTION'){
+    let HOST = "192.168.160.231"
+    console.log(MEDEX_API_BASE_URL);
+    MEDEX_API_BASE_URL = `https://${HOST}:8080/api/v1/`;
+}
+else {
+    MEDEX_API_BASE_URL = 'http://localhost:8080/api/v1/';
+}
